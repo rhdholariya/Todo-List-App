0cc32d14057f28615c966b26d64ec1b6d7fb5d1a
rhdholariya <rahuldholariya001@gmail.com>
2024-07-31 10:31:14 +0530

    Make URL changes

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 require("dotenv").config();
 const connectDB = () => {
-    mongoose.connect(process.env.BASE_URL)
-        .then(() => console.log("Database connected successfully"))
-        .catch((err) => {
-            console.error("Database failed to connect",err.message);
-        });
-}
+  mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("Database connected successfully"))
+    .catch((err) => {
+      console.error("Database failed to connect", err.message);
+    });
+};
 
 module.exports = connectDB;
